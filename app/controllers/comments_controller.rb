e333b431a9a3fee10917f1dcc8f060756c1bc48f
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-01-16 13:03:52 +0100

    built comment to article

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,13 @@
 class CommentsController < ApplicationController
+
+  def create
+    @article = Article.find(params[:article_id])
+    @comment = @article.comments.create(comment_params)
+    redirect_to article_path(@article)
+  end
+
+
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
