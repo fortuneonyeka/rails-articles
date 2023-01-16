365564e9d81ae0c47bf38b6c7e97b6c27c3083aa
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-01-16 13:33:04 +0100

    add status to article and comments with concern module

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,6 @@ class CommentsController < ApplicationController
 
 
   def comment_params
-    params.require(:comment).permit(:commenter, :body)
+    params.require(:comment).permit(:commenter, :body, :status)
   end
 end
